5b87512684e0eb81a79e803becb7222d04042849
TermiNateVader <nathanbenstevens@gmail.com>
2021-05-31 23:25:19 -0400

    Create titlerandom.js

---
+++ js/titlerandom.js
@@ -0,0 +1,3 @@
+var titles = ['TermiNaterVader' 'TermiNateVader' 'TermiNateVader' 'TermiNateVader' 'TermiNateVader' 'TermiNateVader' 'TermiNateVader' 'TermiNateVader' 'TermiNateVader']
+  var title = titles[Math.floor(Math.random() * titles.length)] // pick random item
+  document.title = title
