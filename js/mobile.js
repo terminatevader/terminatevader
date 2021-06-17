54134527463ac052867c33e85bafe56e06d2d3df
TermiNateVader <nathanbenstevens@gmail.com>
2021-06-17 18:53:15 -0400

    Create mobile.js

---
+++ js/mobile.js
@@ -0,0 +1,6 @@
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    content = '<div class="dropdown-content">'
+      content = '<a href="#">Link 1</a>'
+      content = '<a href="#">Link 2</a>'
+      content = '<a href="#">Link 3</a>'
+}
