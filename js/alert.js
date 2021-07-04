1b35a11c1a155d2558ad32cfdeb1b4ee9fa71961
TermiNateVader <nathanbenstevens@gmail.com>
2021-07-03 21:49:07 -0400

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -1,11 +1,11 @@
-var two = localStorage.getItem('two') || '';
-    if (two != 'yes') {
+var three = localStorage.getItem('three') || '';
+    if (three != 'yes') {
      if (confirm("New Blog Entry")==true) {
   location.href = "https://www.terminatevader.com/blog";
 } 
     else{
      alert("Understandable, Have A Great Day")
     }
-      localStorage.setItem('two','yes');
-      localStorage.removeItem("one");
+      localStorage.setItem('three','yes');
+      localStorage.removeItem("two");
     }
