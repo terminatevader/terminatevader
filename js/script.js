8530c1bc34826978118e55c01b264eec4a7aa1c8
TermiNateVader <nathanbenstevens@gmail.com>
2021-08-12 21:42:37 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var visited = localStorage.getItem('visited') || '';
+if(visited != 'yes'){
+    localStorage.setItem('visited','yes');
+}
 var twelve = localStorage.getItem('twelve') || '';
     if (twelve != 'yes') {
      if (confirm("There's a new blog entry!")==true) {
@@ -10,14 +14,14 @@ var twelve = localStorage.getItem('twelve') || '';
       localStorage.removeItem("ten");
 }
 
-var a = localStorage.getItem('a') || '';
-    if (a != 'yes') {
+var b = localStorage.getItem('b') || '';
+    if ((b != 'yes')&&(visited == 'yes')) {
      if (confirm("There's a new update!")==true) {
         location.href="https://github.com/terminatevader/terminatevader.github.io/blob/main/patchnotes.txt";
 } 
         else{
      alert("Understandable, have a great day!");
     }
-      localStorage.setItem('a','yes');
-      localStorage.removeItem("ten");
+      localStorage.setItem('b','yes');
+      localStorage.removeItem("a");
 }
