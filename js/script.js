411399a65e7e94aa95561b2cc0039e7a676fc6c6
TermiNateVader <nathanbenstevens@gmail.com>
2021-08-18 13:09:10 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var visited = localStorage.getItem('visited') || '';
 if(visited != 'yes'){
     localStorage.setItem('visited','yes');
+    localStorage.setItem('c','yes');
 }
 var seventeen = localStorage.getItem('seventeen') || '';
     if (seventeen != 'yes') {
@@ -14,14 +15,14 @@ var seventeen = localStorage.getItem('seventeen') || '';
       localStorage.removeItem("fifteen");
 }
 
-var b = localStorage.getItem('b') || '';
-    if ((b != 'yes')&&(visited == 'yes')) {
+var c = localStorage.getItem('c') || '';
+    if ((c != 'yes')&&(visited == 'yes')) {
      if (confirm("There's a new update!")==true) {
         location.href="https://github.com/terminatevader/terminatevader.github.io/blob/main/patchnotes.txt";
 } 
         else{
      alert("Understandable, have a great day!");
     }
-      localStorage.setItem('b','yes');
-      localStorage.removeItem("a");
+      localStorage.setItem('c','yes');
+      localStorage.removeItem("b");
 }
