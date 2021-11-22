a2e74aa6834da2e0502b08802bb2c77ca1fdb2ae
TermiNateVader <nathanbenstevens@gmail.com>
2021-11-22 16:20:44 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
-var visited = localStorage.getItem('visited') || '';
-if(visited != 'yes'){
-    localStorage.setItem('visited','yes');
-var i = localStorage.getItem('i')
-}
-
-
-    if ((i != 'yes')&&(visited == 'yes')) {
-     if (confirm("There's a new update!")==true) {
-         window.open("https://www.terminatevader.com/patchnotes.txt", '_blank').focus();
-} 
-        else{
-     alert("Understandable, have a great day!");
-    }
-      localStorage.setItem('i','yes');
-      localStorage.removeItem("h");
-}
- if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
     window.location = 'microsoft-edge:' + window.location;
     setTimeout(function() {
       window.location = 'https://go.microsoft.com/fwlink/?linkid=2135547';
