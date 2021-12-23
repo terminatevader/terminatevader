3dff37a846bd7970fba0b6ee5ed9f9db110a47a0
TermiNateVader <nathanbenstevens@gmail.com>
2021-12-23 13:00:47 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
+    alert("Internet Explorer is no longer supported");
     window.location = 'microsoft-edge:' + window.location;
     setTimeout(function() {
       window.location = 'https://go.microsoft.com/fwlink/?linkid=2135547';
