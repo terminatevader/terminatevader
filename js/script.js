884417dfd0bafe39d84153335d99a357db0e37d7
TermiNateVader <nathanbenstevens@gmail.com>
2022-02-12 16:18:25 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,11 +6,3 @@ navigator.serviceWorker.register('/service-worker.js')
     .catch(() => console.log('service worker not registered'))
 }
 
-if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-var r = Math.floor((Math.random() * 101)+1);
-
-if (r == 1)
-{
-    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-}
-}
