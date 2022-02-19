581ac3fe789a4ff1987ab14625525d70013ad057
TermiNateVader <nathanbenstevens@gmail.com>
2022-02-19 16:09:45 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,14 +8,19 @@ navigator.serviceWorker.register('/service-worker.js')
 localStorage.setItem("experiment", "no");
 var experiment = localStorage.getItem("experiment"); 
 
- if (confirm("Press OK to enable experimental mode")) {
+ if (confirm("Press OK to enable experimental mode. Press Cancel to turn off experimental mode")) {
+     localStorage.setItem("experiment","yes");
+if(experiment == "yes"){     
 document.getElementById("TNV").style.display = "none";
 document.getElementById("experiment").style.display = "block";
+}
  }
 else{
+    localStorage.getItem("experiment", "no");
 if (experiment == "no")
 {
 document.getElementById("experiment").style.display = "none";
 document.getElementById("TNV").style.display = "block";
+
 }
 }
