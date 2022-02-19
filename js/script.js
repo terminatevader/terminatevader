4dbfae9fae6454fe7e29408617a0f13a536d3281
TermiNateVader <nathanbenstevens@gmail.com>
2022-02-19 16:06:08 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,4 +5,17 @@ navigator.serviceWorker.register('/service-worker.js')
     .then(() => console.log('service worker registered'))
     .catch(() => console.log('service worker not registered'))
 }
+localStorage.setItem("experiment", "no");
+var experiment = localStorage.getItem("experiment"); 
 
+ if (confirm("Press OK to enable experimental mode")) {
+document.getElementById("TNV").style.display = "none";
+document.getElementById("experiment").style.display = "block";
+ }
+else{
+if (experiment == "no")
+{
+document.getElementById("experiment").style.display = "none";
+document.getElementById("TNV").style.display = "block";
+}
+}
