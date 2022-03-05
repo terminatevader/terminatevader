9260e91cd852b64039dd26f2170c80b51791f488
TermiNateVader <nathanbenstevens@gmail.com>
2022-03-04 22:32:12 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ else if (/Android|iPhone|iPad|iPod|BlackBerry|Opera Mini|Windows Phone|Mobile Sa
 }
 
 
-else (/Windows|Mac|Thunderstorm|i686|Ubuntu|CrOS/i.test(navigator.userAgent)){
+else if (/Windows|Mac|Thunderstorm|i686|Ubuntu|CrOS/i.test(navigator.userAgent)){
     dropdown.style.display = "none";
     bottomnav1.style.display = "none";
     dropbtn.style.display = "none";
