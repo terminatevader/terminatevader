bf07a6da52289b33b58fdeb79a72b6c3b9fddfc8
TermiNateVader <nathanbenstevens@gmail.com>
2022-03-05 13:39:36 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -33,12 +33,7 @@ var dropbtn = document.getElementById('dropbtn');
 var platform = document.getElementById('platform')
 
 
- if (/Xbox/i.test(navigator.userAgent){
-location.href = "https://www.terminatevader.com/unavailable.txt"
-
-}
-
-else if (/Android|iPhone|iPad|iPod|BlackBerry|Opera Mini|Windows Phone|Mobile Safari|Nokia/i.test(navigator.userAgent)){
+if (/Android|iPhone|iPad|iPod|BlackBerry|Opera Mini|Windows Phone|Mobile Safari|Nokia/i.test(navigator.userAgent)){
     topnav.style.display = "none";
     topnav1.style.display = "none";
     bottomnav.style.display = "none";
