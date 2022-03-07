62dbb311972d9096c1eca2c2bcfaf741574830fb
TermiNateVader <nathanbenstevens@gmail.com>
2022-03-06 19:29:11 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
+document.onclick = hideMenu;
+		document.oncontextmenu = rightClick;
 
+		function hideMenu() {
+			document.getElementById(
+				"contextMenu").style.display = "none"
+		}
+
+		function rightClick(e) {
+			e.preventDefault();
+
+			if (document.getElementById(
+				"contextMenu").style.display == "block")
+				hideMenu();
+			else {
+				var menu = document
+					.getElementById("contextMenu")
+					
+				menu.style.display = 'block';
+				menu.style.left = e.pageX + "px";
+				menu.style.top = e.pageY + "px";
+			}
+		}
 if ('serviceWorker' in navigator){
 navigator.serviceWorker.register('/service-worker.js')
     .then(() => console.log('service worker registered'))
