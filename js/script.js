d75cb69f5d291b0d8a275f1138b77a0d8517398c
TermiNateVader <nathanbenstevens@gmail.com>
2022-03-18 22:01:55 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,6 +24,23 @@ document.getElementById("TNV").style.display = "block";
 
 }
 }*/
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
 var topnav = document.getElementById('topnav');
 var topnav1 = document.getElementById('topnav1');
 var bottomnav = document.getElementById('bottomnav');
@@ -75,24 +92,7 @@ else if (/Windows|Mac|Thunderstorm|i686|Ubuntu|CrOS/i.test(navigator.userAgent))
 				menu.style.top = e.pageY + "px";
 			}
 		}
-	var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
-    
+	
 
 }
 
