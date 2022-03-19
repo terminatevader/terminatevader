2deb4696160d5092ca76e2c2e096cc43f4ed1594
TermiNateVader <nathanbenstevens@gmail.com>
2022-03-18 21:32:21 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -75,6 +75,23 @@ else if (/Windows|Mac|Thunderstorm|i686|Ubuntu|CrOS/i.test(navigator.userAgent))
 				menu.style.top = e.pageY + "px";
 			}
 		}
+	var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
     
 
 }
