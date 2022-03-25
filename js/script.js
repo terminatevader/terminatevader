451e433edbc8180dbb7689639de5789ec0190a12
TermiNateVader <nathanbenstevens@gmail.com>
2022-03-25 15:50:23 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator){
 navigator.serviceWorker.register('https://www.terminatevader.com/service-worker.js')
-    .then(() => console.log('service worker registered'))
+   .then(() => console.log('service worker registered'))
     .catch(() => console.log('service worker not registered'))
 }
 
