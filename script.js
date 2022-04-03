1c03a3d2912564fa6ecb8dce678299b2a110c432
TermiNateVader <nathanbenstevens@gmail.com>
2022-04-03 14:45:41 -0400

    Update and rename js/script.js to script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator){
-navigator.serviceWorker.register('https://www.terminatevader.com/service-worker.js')
+navigator.serviceWorker.register('/service-worker.js')
    .then(() => console.log('service worker registered'))
     .catch(() => console.log('service worker not registered'))
 }
