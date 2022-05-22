7bec3c5a82a82266683b5e55f9b1f0a0f2510ecb
TermiNateVader <nathanbenstevens@gmail.com>
2022-05-21 20:33:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,29 @@ showDivs(slideIndex);
 function plusDivs(n) {
   showDivs(slideIndex += n);
 }
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            plusDivs(1)
+        } else {
+           plusDivs(-1)
+        }    
 document.addEventListener('keydown', function(event) {
     if(event.keyCode == 37) {
         plusDivs(-1)
