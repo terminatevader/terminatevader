da38a1316589d9ff6c297cefde7b79b704d9bf6f
TermiNateVader <nathanbenstevens@gmail.com>
2022-07-01 11:03:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -56,4 +56,4 @@ function showDivs(n) {
 
 if(/Trident/i.test(navigator.userAgent)){
   // true for mobile device
-  ("Internet Explorer is no longer supported but you can continue to use the website on Internet Explorer
+  Alert("Internet Explorer is no longer supported but you can continue to use the website on Internet Explorer");
