70c435406b72c44c353ad6d24b8e1abb1fd2032e
TermiNateVader <nathanbenstevens@gmail.com>
2022-07-14 13:14:42 -0400

    Update script.js

--- Extensions/Experimental Mode/script.js
+++ Extensions/Experimental Mode/script.js
@@ -1,8 +1,1 @@
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
 
-today = mm + '/' + dd + '/' + yyyy;
-const topnav = document.getElementById("topnav");
-topnav.textContent = today;
