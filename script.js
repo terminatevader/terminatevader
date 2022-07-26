c4953ac1e87783913f3eda9d581823c3e7194a0c
TermiNateVader <nathanbenstevens@gmail.com>
2022-07-26 18:21:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log(navigator.userAgent);
 // Simulate an HTTP redirect:
-window.location.replace("http://www.terminatevader.com/broken");
+window.location.replace("http://www.terminatevader.com/broken.txt");
 
 
   if (typeof(Storage) !== "undefined") {
