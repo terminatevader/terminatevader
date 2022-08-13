4546247e551ab8cd52094ed2a67b16d4bf92e0f8
TermiNateVader <nathanbenstevens@gmail.com>
2022-08-13 11:37:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -55,10 +55,4 @@ function showDivs(n) {
   x[slideIndex-1].style.display = "block";  
 }
 
-  if(/MSIE \d|Trident.*rv:/.test(navigator.userAgent)) {
-    window.location = 'microsoft-edge:' + window.location;
-    setTimeout(function() {
-      window.location = 'https://go.microsoft.com/fwlink/?linkid=2135547';
-    }, 1);
-  }
 
