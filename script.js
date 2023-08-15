e800f366126fe6caa4029c44fb42df29ad3b0940
TermiNateVader <nathanbenstevens@gmail.com>
2023-08-15 19:49:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ if(/wv/i.test(navigator.userAgent)){
 			
 	const topnav = document.getElementById("topnav");
 
-	topnav.textContent = "TNV Universe";
+	topnav.textContent = "TermiNateVader";
 var slideIndex = 1;
 showDivs(slideIndex);
 
