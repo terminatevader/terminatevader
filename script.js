35b3cafc0aa99d907588c966564b393baba7a74e
TermiNateVader <nathanbenstevens@gmail.com>
2023-08-18 17:32:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,62 +4,7 @@ if(/Android/i.test(navigator.userAgent) && !/wv/i.test(navigator.userAgent)){
 	
 
 }
-if(/wv/i.test(navigator.userAgent) && (/Android/i.test(navigator.userAgent)){
-window.location.replace("https://www.terminatevader.com/myplanforaftercollege");
-	
-	myElement.addEventListener("touchstart", startTouch, false);
-myElement.addEventListener("touchmove", moveTouch, false);
- 
-// Swipe Up / Down / Left / Right
-var initialX = null;
-var initialY = null;
- 
-function startTouch(e) {
-  initialX = e.touches[0].clientX;
-  initialY = e.touches[0].clientY;
-};
- 
-function moveTouch(e) {
-  if (initialX === null) {
-    return;
-  }
- 
-  if (initialY === null) {
-    return;
-  }
- 
-  var currentX = e.touches[0].clientX;
-  var currentY = e.touches[0].clientY;
- 
-  var diffX = initialX - currentX;
-  var diffY = initialY - currentY;
- 
-  if (Math.abs(diffX) > Math.abs(diffY)) {
-    // sliding horizontally
-    if (diffX > 0) {
-      // swiped left
-      console.log("swiped left");
-    } else {
-      // swiped right
-      console.log("swiped right");
-    }  
-  } else {
-    // sliding vertically
-    if (diffY > 0) {
-      // swiped up
-      window.location.replace("https://www.terminatevader.com/cooldogs");
-    } else {
-      // swiped down
-      console.log("swiped down");
-    }  
-  }
- 
-  initialX = null;
-  initialY = null;
-   
-  e.preventDefault();
-};
-}
+
 	console.log(navigator.userAgent);
 	document.addEventListener('keydown', function(event) {
     if(event.keyCode == 37) {
