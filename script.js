fac7c0d1b67ff66c218e1081f9490e56ccdddb07
TermiNateVader <nathanbenstevens@gmail.com>
2023-08-22 17:46:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ const topnav = document.getElementById("topnav");
 
 
 	console.log(navigator.userAgent);
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -39,15 +49,6 @@ function plusDivs(n) {
 
 
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
-}
+
 
 
