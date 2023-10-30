6577863dbafa17d3660eb095ecac9ba64f2044f4
TermiNateVader <nathanbenstevens@gmail.com>
2023-10-30 09:29:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,27 +11,7 @@ window.addEventListener("offline", function() {
     
 
 	
-function preloadImages(array) {
-    if (!preloadImages.list) {
-        preloadImages.list = [];
-    }
-    var list = preloadImages.list;
-    for (var i = 0; i < array.length; i++) {
-        var img = new Image();
-        img.onload = function() {
-            var index = list.indexOf(this);
-            if (index !== -1) {
-                // remove image from the array once it's loaded
-                // for memory consumption reasons
-                list.splice(index, 1);
-            }
-        }
-        list.push(img);
-        img.src = array[i];
-    }
-}
 
-preloadImages(["https://www.terminatevader.com/images/Florida.jpg", "https://www.terminatevader.com/images/hank.jpg", "https://www.terminatevader.com/images/legoland2.jpg", "https://www.terminatevader.com/images/gatlinburgwater.jpg"]);
 
 
 	console.log(navigator.userAgent);
