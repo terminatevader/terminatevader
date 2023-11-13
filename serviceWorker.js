12e6537da7ca5f2a122094d1b8635fa7ba0e17fc
TermiNateVader <nathanbenstevens@gmail.com>
2023-11-13 12:46:54 -0500

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -12,3 +12,10 @@ self.addEventListener("install", installEvent => {
   )
 })
 
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
