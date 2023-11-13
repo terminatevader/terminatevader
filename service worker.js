fb45602bbf553ce9f4ef874b0e54ab52dc520e23
TermiNateVader <nathanbenstevens@gmail.com>
2023-11-13 12:44:08 -0500

    Update service worker.js

--- service worker.js
+++ service worker.js
@@ -1,13 +1,1 @@
-const staticDevCoffee = "TermiNateVader"
-const assets = [
-  "/",
-  "/index.html",
-]
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
