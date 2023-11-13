c1ad117509f48a5bcf44a47ae85293d7d1fd0e2e
TermiNateVader <nathanbenstevens@gmail.com>
2023-11-13 15:44:54 -0500

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,10 @@
-const TNV = "TermiNateVader"
-const assets = [
-  "/",
+
+
+// Establish a cache name
+const cacheName = 'MyFancyCacheName_v1';
+
+// Assets to precache
+const precachedAssets = [
   "/index.hhtml",
   "/cooldogs.html",
   "/myexperienceatlegolandflorida.html",
@@ -35,21 +39,28 @@ const assets = [
 "/images/gatlinburgwater.jpg",
   "/images/legoland2.jpg",
 "/images/Legoland3.jpg",
-  "/images/gatlinburg.png",
-]
+  "/images/gatlinburg.png"
+];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(TNV).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
+self.addEventListener('install', (event) => {
+  // Precache assets on install
+  event.waitUntil(caches.open(cacheName).then((cache) => {
+    return cache.addAll(precachedAssets);
+  }));
+});
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
-})
+self.addEventListener('fetch', (event) => {
+  // Is this one of our precached assets?
+  const url = new URL(event.request.url);
+  const isPrecachedRequest = precachedAssets.includes(url.pathname);
+
+  if (isPrecachedRequest) {
+    // Grab the precached asset from the cache
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      return cache.match(event.request.url);
+    }));
+  } else {
+    // Go to the network
+    return;
+  }
+});
